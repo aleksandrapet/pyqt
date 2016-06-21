d40b9a6ed7bc452a49eb8e3896b523604681f7ae
aleksandrapet <aleksandra@fidko.com>
2016-06-21 08:48:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,21 +5,43 @@ import json
 import PyQt4
 from PyQt4.QtGui import * 
 from PyQt4.QtCore import *
+from PyQt4 import QtGui, QtCore
 
+
+import pfqt
 
 import time
 import os
 import RPi.GPIO as GPIO
 import mainwindow_auto
 
-#GPIO.setmode(GPIO.BCM)
-ser=serial.Serial("/dev/ttyAMA0", 115200, timeout=0.1)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-#decoded = json.loads(json_input)
 
+ser=serial.Serial("/dev/ttyAMA0", 115200, timeout=0.5)
 
-class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
+#while True:
+    #json_input = ser.readline()
+    #decoded = json.loads(json_input)
+    #print (decoded['T_plate_avg'])
         
+class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):     
+
+    def presflow(self):
+	pres,flo=pfqt.onetime()
+	
+	
+	self.label_27.setText(str(pres))
+	self.label_29.setText(str(flo))	  
+          
     def btnstate(self):
         if self.wheel_button.isChecked():
             self.wheel_button.setText("Wheel Open")
@@ -28,7 +50,7 @@ class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
             json_out=json.dumps(a)
             print(json_out)
             ser.write(json_out)
-            
+                        
         else:
             self.wheel_button.setText("Wheel Closed")
             print("Closing wheel")
@@ -37,38 +59,40 @@ class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
             print(json_out)
             ser.write(json_out)
 
-    def serialRead(self):
-        if ser.isOpen():
-            try:
-                json_input=ser.readline()
-                decoded = json.loads(json_input)
-                print "Plate temperature", decoded['T_plate_avg']
-                self.T_plate.setNum(decoded['T_plate_avg'])
-            except:
-                print "ERROR"
+   
+    def tabSelected(self, arg=None):
+        print '\n\t tabSelected() current Tab index =', arg
 
-        #def pressedW_close(self):
-        #print("Closing wheel")
+    def whatTab(self):
+        currentIndex=self.tabWidget.currentIndex()
+        currentWidget=self.tabWidget.currentWidget()
+
+        print '\n\t Query: current Tab index =', currentIndex
+
 
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+                    
+       
+	self.tabWidget.connect(self.tabWidget, SIGNAL("currentChanged(int)"), self.tabSelected)
 
-    
-        #json_input = ser.read()
-        #decoded = json.loads(json_input)
-        #print(decoded['T_plate'])
-        
         self.wheel_button.setCheckable(True)
         self.wheel_button.clicked.connect(lambda: self.btnstate())
-        #self.T_plate.connect(self.serialRead())
-            
+	
+	pres,flo=pfqt.onetime()
+	
+	
+	self.label_27.setText(str(pres))
+	self.label_29.setText(str(flo))
+                    
 def main():
     app = QApplication(sys.argv)
     form = MainWindow()
     form.show()
-
+    
     sys.exit(app.exec_())
 
 if __name__=="__main__":
     main()
+
---
+++ mcp3008_pf.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+#--------------------------------------   
+# This script reads data from a 
+# MCP3008 ADC device using the SPI bus.
+#
+# Author : Matt Hawkins
+# Date   : 13/10/2013
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+
+import spidev
+import time
+import os
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+# Function to read SPI data from MCP3008 chip
+# Channel must be an integer 0-7
+def ReadChannel(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+
+# Function to convert data to voltage level,
+# rounded to specified number of decimal places. 
+def ConvertVolts(data,places):
+  volts = (data * 3.3) / float(1023)
+  volts = round(volts,places)  
+  return volts
+  
+# Function to calculate pressure from
+# TMP36 data, rounded to specified
+# number of decimal places.
+def ConvertPress(data,places):
+ 
+  press = ((data * 17.05)/float(1023))-2.58
+  press = round(press,places)
+  
+  if press<0:
+
+	press=0
+
+  return press
+
+
+def ConvertFlow(data,places):
+
+  flow = ((data * 1.52)/float(1023))-0.22
+  flow = round(flow,places)
+
+  if flow<0:
+		
+	flow=0
+
+  return flow
+  
+# Define sensor channels
+flow_channel = 0
+press_channel  = 1
+
+# Define delay between readings
+delay = 1
+
+while True:
+
+  # Read the flow sensor data
+  flow_level = ReadChannel(flow_channel)
+  flow_volts = ConvertVolts(flow_level,2)
+  flow       = ConvertFlow(flow_level,2)
+
+  # Read the pressure sensor data
+  press_level = ReadChannel(press_channel)
+  press_volts = ConvertVolts(press_level,2)
+  press       = ConvertPress(press_level,2)
+
+  # Print out results
+  print "--------------------------------------------"  
+  print("Flow      : {} ({}V) {} l".format(flow_level,flow_volts,flow))  
+  print("Pressure  : {} ({}V) {} bar".format(press_level,press_volts,press))    
+
+  # Wait before repeating loop
+  time.sleep(delay)
+ 
